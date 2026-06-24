8676ecfd8246083983f7efab52353009967767c5
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-06-24 16:52:02 +0200

    Solve phonebook step 2: prevent from adding names that already exists

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -12,7 +12,12 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPersons(persons.concat({ name: newName }))
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPerson = { name: newName };
+      setPersons(persons.concat(newPerson));
+    }
   }
 
   return (
