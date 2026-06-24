e2469d39b7423b12aedcf15e026940eba72c3e53
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-06-24 16:30:31 +0200

    Solve phonebook step 1: create a phonebook

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleInput = (e) => {
+    setNewName(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPersons(persons.concat({ name: newName }))
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handleInput}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => <div key={person.name}>{person.name}</div>)}
+    </div>
+  )
+}
+
+export default App
