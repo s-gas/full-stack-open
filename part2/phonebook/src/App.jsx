63e3610c0bdb5418176e43c828556666c0178af6
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-06-25 11:12:45 +0200

    Solve phonebook step 6: get initial data from the server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567',
-    }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(res => setPersons(res.data));
+  }, [])
 
   const filtered = persons.filter((person) => 
     person.name.toLowerCase().includes(filter.toLowerCase(), 0)
