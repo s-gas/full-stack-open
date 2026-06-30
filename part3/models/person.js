cc55acb4f3495660feea89d1e2f1011fd5ce507c
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-06-30 21:34:05 +0200

    Solve phonebook database step 1: Fetch data from db

--- part3/models/person.js
+++ part3/models/person.js
@@ -14,6 +14,14 @@ const schema = new mongoose.Schema(
   }
 );
 
+schema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = document._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
 const Person = mongoose.model('Person', schema);
 
 module.exports = Person;
