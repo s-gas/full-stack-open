62b1a3486873ff3e44b5d143354b82dd081a17ae
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-06 14:14:28 +0200

    Solve phonebook database step 4: user error handler middleware

--- part3/index.js
+++ part3/index.js
@@ -13,13 +13,24 @@ morgan.token('body', function (req) {
 
 const persons = [];
 
+const errorHandler = (error, req, res, next) => {
+  console.error(error);
+
+  if (error.name === "CastError") {
+    res.status(400).json({error: "malformatted id"});
+    return;
+  }
+
+  next(error);
+}
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((people) => res.json(people));
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
   Person
     .findById(id)
@@ -27,10 +38,7 @@ app.get("/api/persons/:id", (req, res) => {
       if (person) res.json(person);
       else res.status(404).json({error: "not found"});
     })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).json({error: "malformatted id"});
-    });
+    .catch((error) => next(error)); //next with an argument jumps to the first error-handling middleware -> func(error, req, res, next)
 });
 
 app.post("/api/persons", (req, res) => {
@@ -50,17 +58,14 @@ app.post("/api/persons", (req, res) => {
   });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
   Person
     .findByIdAndDelete(id)
     .then(() => {
       res.status(204).end();
     })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).json({error: "malformatted id"});
-    });
+    .catch((error) => next(error));
 });
 
 app.get("/info", (req, res) => {
@@ -70,6 +75,8 @@ app.get("/info", (req, res) => {
   `<div>${info}</div><div>${new Date().toString()}</div>` 
   )
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
