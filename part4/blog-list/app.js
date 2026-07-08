3574c570fb41e721d37cd743a627406ad40bc91b
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-08 14:05:48 +0200

    Solve blog list step 2: Refactor app into separate modules

---
+++ part4/blog-list/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose
+  .connect(config.MONGODB_URL, { family: 4 })
+  .then(() => console.log("Connected to db"))
+  .catch(() => console.log("Failed to connect to db"));
+
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app
