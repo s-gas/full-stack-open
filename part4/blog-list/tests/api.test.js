c26eef3ec1ba5edb39832aac364e5e1c3b8037f2
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-09 14:27:04 +0200

    Test that with empty db GET responds with an empty array

--- part4/blog-list/tests/api.test.js
+++ part4/blog-list/tests/api.test.js
@@ -1,14 +1,30 @@
-const app = require('../app')
 const mongoose = require('mongoose')
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
 const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+})
 
 test('200 for GET requests', async () => {
   await api
         .get('/api/blogs')
         .expect(200)
+})
+
+test('empty array with empty db', async () => {
+  await api
+        .get('/api/blogs')
+        .expect(200)
+
+  const res = await api.get('/api/blogs');
+  
+  assert.strictEqual(res.body.length, 0);
 })
 
 after(async () => {
