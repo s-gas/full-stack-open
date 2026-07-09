4b41d811d6c5d8890f17dbd39792561131f178ef
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-09 15:45:23 +0200

    Solve blog list tests step 3: test POST request

--- part4/blog-list/tests/api.test.js
+++ part4/blog-list/tests/api.test.js
@@ -64,3 +64,51 @@ test('GET request returns entries with "id"', async () => {
 after(async () => {
   await mongoose.connection.close();
 })
+
+test('POST request returns 201', async () => {
+  const requestBody = {
+    title: "x",
+    author: "x",
+    url: "x",
+    likes: 2
+  }
+
+  await api
+          .post('/api/blogs')
+          .send(requestBody)
+          .expect(201)
+})
+
+test('POST request saves entry in db', async () => {
+  const requestBody = {
+    title: "x",
+    author: "x",
+    url: "x",
+    likes: 2
+  }
+
+  await api
+          .post('/api/blogs')
+          .send(requestBody)
+
+  const res = await api.get('/api/blogs');
+
+  assert(res.body.length);
+})
+
+test('POST request saves entry with right fields', async () => {
+  const requestBody = {
+    title: "x",
+    author: "x",
+    url: "x",
+    likes: 2
+  }
+
+  await api
+          .post('/api/blogs')
+          .send(requestBody)
+
+  const res = await api.get('/api/blogs')
+
+  assert.strictEqual(requestBody.title, res.body[0].title);
+})
