c02735c9a1919463e3aab8b7827b2f2a37eb8220
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-15 10:10:03 +0200

    Respond with JWT to POST request at /api/login

--- part4/blog-list/controllers/login.js
+++ part4/blog-list/controllers/login.js
@@ -1,10 +1,32 @@
 const loginRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const config = require('../utils/config')
 
 loginRouter.post('/', async (req, res) => {
   const {username, password} = req.body;
 
-  console.log(username)
-  console.log(password)
+  const user = await User.findOne({username})
+
+  if (!user) {
+    return res.status(401).json({error: "invalid username"});
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.hashedPassword);
+
+  if (!isPasswordCorrect) {
+    return res.status(401).json({error: "invalid password"});
+  }
+
+  const tokenPayload = {
+    username: user.username,
+    id: user._id,
+  }
+
+  const token = jwt.sign(tokenPayload, config.SECRET);
+
+  res.json({token, username: user.username});
 })
 
 module.exports = loginRouter;
