0016e0f3f46f10b785c0b2ba99d1a2e25b05bd4a
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-21 21:32:45 +0200

    Solve 5.7 Blog List Frontend, step 7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,21 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState} from 'react'
+
+const Blog = ({ blog }) => {
+  const [isShown, setIsShown] = useState(false);
+  
+  return (
+    <div>
+      <span>{blog.title} {blog.author}</span>
+      <button onClick={() => setIsShown(!isShown)}>{isShown ? "hide" : "view"}</button>
+      {isShown && 
+        <>
+          <div>{blog.url}</div>
+          <div>{blog.likes}</div>
+          <div>{blog.user.name}</div>
+        </>
+      }
+    </div>
+  )
+}
 
 export default Blog
