904f0b9ef10a02562542ae1afc236acf73478563
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-21 13:22:57 +0200

    Create login form

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -4,6 +4,8 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     (async() => {
@@ -12,8 +14,29 @@ const App = () => {
     })();
   }, [])
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  }
+
   return (
     <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            username
+            <input onChange={(e) => setUsername(e.target.value)}/>
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input onChange={(e) => setPassword(e.target.value)}/>
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
