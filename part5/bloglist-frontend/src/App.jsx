4659fcfbc6409e484fce40b70032825d16c396e5
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-21 13:39:20 +0200

    Add conditional rendering

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     (async() => {
@@ -14,34 +16,50 @@ const App = () => {
     })();
   }, [])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("submit");
+    try {
+      const user = await loginService.login({username, password});
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
-    <div>
-      <h2>log in to application</h2>
-      <form onSubmit={handleSubmit}>
+    <>
+      {!user && 
         <div>
-          <label>
-            username
-            <input onChange={(e) => setUsername(e.target.value)}/>
-          </label>
+          <h2>log in to application</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>
+                username
+                <input onChange={(e) => setUsername(e.target.value)}/>
+              </label>
+            </div>
+            <div>
+              <label>
+                password
+                <input type="password" onChange={(e) => setPassword(e.target.value)}/>
+              </label>
+            </div>
+            <button type="submit">login</button>
+          </form>
         </div>
+      }
+      {user &&
         <div>
-          <label>
-            password
-            <input onChange={(e) => setPassword(e.target.value)}/>
-          </label>
+          <h2>blogs</h2>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
         </div>
-        <button type="submit">login</button>
-      </form>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+      }
+    </>
   )
 }
 
