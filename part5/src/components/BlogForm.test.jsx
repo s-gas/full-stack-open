ea43fc177356c6f7a88079a74d4166bab9594ddb
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-24 15:23:47 +0200

    Solve 5.16: Blog List Tests, step 4

--- part5/src/components/BlogForm.test.jsx
+++ part5/src/components/BlogForm.test.jsx
@@ -2,8 +2,19 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import BlogForm from './BlogForm'
 
-test('event handler called with right details', () => {
-  
+test('event handler called with right details', async () => {
+  const createBlog = vi.fn();
 
+  const user = userEvent.setup();
 
+  render(<BlogForm createBlog={createBlog} />);
+
+  const input = screen.getByLabelText('title:')
+  const button = screen.getByText('create')
+
+  await user.type(input, "hello");
+  await user.click(button);
+
+  expect(createBlog.mock.calls).toHaveLength(1);
+  expect(createBlog.mock.calls[0][0]).toBe("hello");
 });
