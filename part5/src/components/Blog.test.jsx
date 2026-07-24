7266845f6957a1be0a4c8e042f9407248a2f3610
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-24 13:35:26 +0200

    Solve 5.14: Blog List Tests, step 2

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders only title and author', () => {
@@ -7,9 +8,18 @@ test('renders only title and author', () => {
     author: "author",
     url: "url",
     likes: 1,
+    user: {
+      username: "username",
+      name: "name",
+    },
   }
 
-  render(<Blog blog={blog} />)
+  const user = {
+    username: "username",
+    name: "name",
+  }
+
+  render(<Blog blog={blog} user={user}/>)
 
   const titleAndAuthor = screen.getByText("title author");
 
@@ -22,4 +32,37 @@ test('renders only title and author', () => {
   const likes = screen.queryByText(1);
 
   expect(likes).toBeNull();
+})
+
+test('url and likes visible after button click', async () => {
+  const blog = {
+    title: "title",
+    author: "author",
+    url: "url",
+    likes: 1,
+    user: {
+      username: "username",
+      name: "name",
+    },
+  }
+
+  const user = {
+    username: "username",
+    name: "name",
+  }
+
+  render(<Blog blog={blog} user={user}/>)
+
+
+  const u = userEvent.setup();
+  const button = screen.getByText("view");
+
+  await u.click(button);
+
+  const url = screen.getByText("url");
+  const likes = screen.getByText("likes 1");
+
+  expect(button).toBeDefined();
+  expect(url).toBeDefined();
+  expect(likes).toBeDefined();
 })
