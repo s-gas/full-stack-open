13842c90c47811c77a6e6bca3ce6f82cbd59177a
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-28 11:08:44 +0200

    Solve 5.18: Blog List End To End Testing, step 2

--- part5/tests/tests/blog_app.spec.js
+++ part5/tests/tests/blog_app.spec.js
@@ -3,7 +3,14 @@ import { test, expect } from '@playwright/test';
 
 test.describe("Blog app", () => {
 
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page, request }) => {
+    await request.delete('http://localhost:3003/api/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        username: "s-gas",
+        password: "00000000",
+        name: "Simone Gasparini",
+    } })
     await page.goto('http://localhost:5173');
   });
 
@@ -20,6 +27,15 @@ test.describe("Blog app", () => {
 
       const title = page.getByText("blogs");
       await expect(title).toBeVisible();
+    })
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await page.getByLabel("username").fill("s-gas");
+      await page.getByLabel("password").fill("wrong-password");
+      await page.getByText("login").click();
+
+      const errorMessage = page.getByText("wrong username or password");
+      await expect(errorMessage).toBeVisible();
     })
   });
 });
